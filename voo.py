9e8137bf748ee5f408978bd0256a119120f6d5a9
Graciele-Tourinho <gracielet.bbts@gmail.com>
2022-09-12 12:18:25 -0300

    Atualização aula 12_09

--- voo.py
+++ voo.py
@@ -1,6 +1,7 @@
 class Voo():
     '''
-    Classe para representar um vôo aéreo
+    Classe para representar um vôo aéreo junta assento, comissario, passageiro
+    piloto e compra passagem
     '''
     def __init__(self):        
         pass
